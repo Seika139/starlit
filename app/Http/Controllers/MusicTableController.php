6d6f7080ade616a41b5815518980e910807b200a
Seika139 <tcoanzu@gmail.com>
2020-02-02 21:00:02 +0900

    :art: Create MusicTableController
    **create**
    MusicTableController

---
+++ app/Http/Controllers/MusicTableController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MusicTableController extends Controller
+{
+        public function index(){
+            
+        }
+}
